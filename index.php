c084bbde5c8fbc27c9c1ef10f726e2af44196825
rafli4514 <muhammadrafli.tkn@gmail.com>
2023-11-15 00:46:50 +0700

    membuat halaman pemesanan

--- index.php
+++ index.php
@@ -283,7 +283,7 @@
     </section>
 
     <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.025);">
-      © 2021 Copyright:
+
       <p class="text-reset fw-bold">AyoMakan</p>
     </div>
   </footer>
